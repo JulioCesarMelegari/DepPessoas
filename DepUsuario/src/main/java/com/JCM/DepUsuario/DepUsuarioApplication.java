42a57cfc9a2ffec4ccbd53027587d154cb772157
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-01-03 17:22:00 -0300

    projeto criado com entidades

---
+++ DepUsuario/src/main/java/com/JCM/DepUsuario/DepUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.JCM.DepUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepUsuarioApplication.class, args);
+	}
+
+}
---
+++ DepUsuario/src/test/java/com/JCM/DepUsuario/DepUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JCM.DepUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
